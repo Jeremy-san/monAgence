4a2ecf368eb1d4989b1fda97f95d22c3dc82a487
Jeremy-san <shehata.jeremy@gmail.com>
2020-06-30 16:20:39 +0200

    reactive form

--- src/app/admin/admin-properties/admin-properties.component.ts
+++ src/app/admin/admin-properties/admin-properties.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-properties',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPropertiesComponent implements OnInit {
 
-  constructor() { }
+  propertiesForm: FormGroup;
+
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initPorpertiesForm();
   }
 
+  initPorpertiesForm() {
+    this.propertiesForm = this.fb.group({
+      title: ['', Validators.required],
+      category: ['', Validators.required],
+      surface: ['', Validators.required],
+      rooms: ['', Validators.required],
+      description: '',
+      price: ['', Validators.required]
+    });
+  }
+
+  onSubmitPropertiesForm() {
+    console.log(this.propertiesForm.value);
+
+  }
 }
