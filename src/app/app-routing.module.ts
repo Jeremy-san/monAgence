4ca31a0a6d0c84601b5370c00336a413529467e8
Jeremy-san <shehata.jeremy@gmail.com>
2020-07-04 04:44:19 +0200

    secure route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 import { SigninComponent } from './authentication/signin/signin.component';
 import { SinglePropertyComponent } from './components/single-property/single-property.component';
+import { AuthGuardService } from './servies/auth-guard.service';
 
 
 const routes: Routes = [
@@ -11,13 +12,13 @@ const routes: Routes = [
     path: 'home', component: HomeComponent
   },
   {
-    path: 'admin/dashboard', component: AdminDashboardComponent
+    path: 'admin/dashboard', canActivate: [AuthGuardService] , component: AdminDashboardComponent
   },
   {
     path: 'login', component: SigninComponent
   },
   {
-    path: 'property/:id', component: SinglePropertyComponent
+    path: 'property/:id',canActivate: [AuthGuardService], component: SinglePropertyComponent
   },
   {
     path: '', redirectTo: 'home', pathMatch: 'full'
